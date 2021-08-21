2ccfaa5a03da7e6305fad05103b839965c94af70
CptIdea <pliyznik@yandex.ru>
2021-08-21 19:57:56 +0300

    fix tests

--- internal/befunge/executor_test.go
+++ internal/befunge/executor_test.go
@@ -11,17 +11,17 @@ func TestExecuteCode(t *testing.T) {
 		args args
 		want int
 	}{
-		{"zero",args{"@"},0},
-		{"one",args{">1@"},1},
-		{"sum",args{">11+@"},2},
-		{"diff",args{">21-@"},1},
-		{"multiple",args{">22*@"},4},
-		{"division",args{">48/@"},2},
-		{"infinity",args{"><"},0},
+		{"zero", args{"@"}, 0},
+		{"one", args{">1@"}, 1},
+		{"sum", args{">11+@"}, 2},
+		{"diff", args{">21-@"}, 1},
+		{"multiple", args{">22*@"}, 4},
+		{"division", args{">48/@"}, 2},
+		{"infinity", args{"><"}, 0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ExecuteCode(tt.args.code); got != tt.want {
+			if got := ExecuteCode(tt.args.code, []int{}); got != tt.want {
 				t.Errorf("ExecuteCode() = %v, want %v", got, tt.want)
 			}
 		})
